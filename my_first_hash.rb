21a14a3dba180c31680ac844b634b9def0b043a3
Logan Smith <logsmith06@gmail.com>
2019-10-21 22:53:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,7 +19,8 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
+                 # return the shipping_manifest hash below
+               shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
@@ -39,6 +40,5 @@ def adding
 
 
 
-  # return the shipping_manifest hash below
-shipping_manifest["oil paintings"]
+
 end
