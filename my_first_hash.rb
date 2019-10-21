2e35481a11d1faa381daa46c3070cc5f681b648f
Logan Smith <logsmith06@gmail.com>
2019-10-21 22:51:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,7 +38,7 @@ shipping_manifest["gun powder"] = 4
   # add 4 gun powder to the shipping_manifest hash below
 
 
-shipping_manifest["oil paintings"]
-  # return the shipping_manifest hash below
 
+  # return the shipping_manifest hash below
+shipping_manifest["oil paintings"]
 end
